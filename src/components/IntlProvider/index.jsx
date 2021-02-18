2a175de11440afa26f3352c82c81a567ec964c9b
Mesaque Francisco <mesaquenf@gmail.com>
2021-02-18 09:01:52 -0300

    Toggling language

--- src/components/IntlProvider/index.jsx
+++ src/components/IntlProvider/index.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { IntlProvider as Provider } from 'react-intl'
-import { DEFAULT_LOCALE } from '../../Constants'
-import enUS from '../../lang/en-US.json'
+import { DEFAULT_LOCALE, LANGUAGES } from '../../Constants'
+import enUS from 'lang/en-US.json'
+import ptBR from 'lang/pt-BR.json'
+import LanguageContext from 'context/LanguageContext'
 
 const IntlProvider = ({ children }) => {
+  const { language } = useContext(LanguageContext)
+  const [messages, setMessages] = useState(enUS)
+
+  useEffect(() => {
+    const nextMessages = language === LANGUAGES['en-US'] ? enUS : ptBR
+    setMessages(nextMessages)
+  }, [language])
+
   return (
     <Provider
       locale={DEFAULT_LOCALE}
       defaultLocale={DEFAULT_LOCALE}
-      messages={enUS}
+      messages={messages}
     >
       {children}
     </Provider>
